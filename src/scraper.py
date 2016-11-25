7ac444f5152576bd85642a5d7db1ffaaf8e84856
Ben Fichter <benjamin_fichter@alumni.brown.edu>
2016-11-25 12:37:26 -0500

    added parsing of raw string variables

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests
+import re
 
 class Scraper:
     def __init__(self, stationID):
@@ -12,14 +13,15 @@ class Scraper:
         self.tree = html.fromstring(page.content)
         
         # Main box variables
-        windDirection = self.grabFromTree('"Wind Direction (WDIR):"')
-        windSpeed = self.grabFromTree('"Wind Speed (WSPD):"')
-        windGust = self.grabFromTree('"Wind Gust (GST):"')
-        waveHeight = self.grabFromTree('"Wave Height (WVHT):"')
-        dominantPeriod = self.grabFromTree('"Dominant Wave Period (DPD):"')
-        averagePeriod = self.grabFromTree('"Average Period (APD):"')
-        meanWaveDirection = self.grabFromTree('"Mean Wave Direction (MWD):"')
-        airTemperature = self.grabFromTree('"Air Temperature (ATMP):"')
+        windDirection = self.grabDirectionTupleFromString(self.grabFromTree('"Wind Direction (WDIR):"'))
+        windSpeed = self.grabNumberFromString(self.grabFromTree('"Wind Speed (WSPD):"'))
+        windGust = self.grabNumberFromString(self.grabFromTree('"Wind Gust (GST):"'))
+        waveHeight = self.grabNumberFromString(self.grabFromTree('"Wave Height (WVHT):"'))
+        dominantPeriod = self.grabNumberFromString(self.grabFromTree('"Dominant Wave Period (DPD):"'))
+        averagePeriod = self.grabNumberFromString(self.grabFromTree('"Average Period (APD):"'))
+        meanWaveDirection = self.grabDirectionTupleFromString(self.grabFromTree('"Mean Wave Direction (MWD):"'))
+        airTemperature = self.grabNumberFromString(self.grabFromTree('"Air Temperature (ATMP):"'))
+        
         print windDirection
         print windSpeed
         print windGust
@@ -29,7 +31,39 @@ class Scraper:
         print meanWaveDirection
         print airTemperature
         
-    # Grab the raw string from the parsed tree with xPath
     def grabFromTree(self, variableDescription):
-        return self.tree.xpath(self.xPathPrefix +  variableDescription + self.xPathSuffix)
+        valueList = self.tree.xpath(self.xPathPrefix +  variableDescription + self.xPathSuffix)
+        
+        if len(valueList) > 0:
+            return valueList[0]
+        else:
+            return ''
+        
+    # these strings only have one number in them
+    def grabNumberFromString(self, rawString):
+        numberList = re.findall(r"[-+]?\d*\.\d+|\d+", rawString)
+        
+        if len(numberList) > 0:
+            return float(numberList[0])
+        else:
+            return None
+        
+    # the string is like ' SE (direction 320 degrees true) '
+    def grabCompassFromString(self, rawString):
+        strippedString = rawString.replace(" ", "")
+        splitList = strippedString.split('(')
+        
+        if len(splitList) > 0:
+            return splitList[0]
+        else:
+            return None
+    
+    def grabDirectionTupleFromString(self, rawString):
+        compass = self.grabCompassFromString(rawString)
+        velocity = self.grabNumberFromString(rawString)
+        
+        if compass is None or velocity is None:
+            return None
+        else:
+            return (compass, velocity)
     
