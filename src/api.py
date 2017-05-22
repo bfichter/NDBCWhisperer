b79472dd8113cf142616a9b34842d363a92e7b83
Ben Fichter <benjamin_fichter@alumni.brown.edu>
2017-05-21 21:20:58 -0400

    add alert schema and possibly lots of other stuff

--- src/api.py
+++ src/api.py
@@ -2,4 +2,4 @@ from eve import Eve
 app = Eve()
 
 if __name__ == '__main__':
-    app.run(port=5002) # was conflicting with something on 5000
+    app.run(port=5002, debug=True) # was conflicting with something on 5000
--- src/settings.py
+++ src/settings.py
@@ -4,12 +4,13 @@ MONGO_PORT = 27017
 #MONGO_USERNAME = '<your username>'
 #MONGO_PASSWORD = '<your password>'
 MONGO_DBNAME = 'ndbc'
+#DEBUG = True
 
-RESOURCE_METHODS = ['GET']
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
 users = {
-    'item_title': 'user',
+    #'item_title': 'user',
     
     'additional_lookup': {
         'url': 'regex("[\w]+")',
@@ -21,10 +22,39 @@ users = {
             'type': 'string',
             'required': True,
             'unique': True
-        }
+        },
+        'token': {
+            'type': 'string',
+        }       
+    }
+}
+
+rangeType = {
+    'type': 'dict',
+    'schema': {
+        'clockwise_start': {'type': 'double'},
+        'clockwise_end': {'type': 'double'}
+    }
+}
+
+alerts = {
+    'schema': {
+        'number': {
+             'type': 'string',
+             'required': True,
+        },
+        'wind_direction_range': rangeType,
+        'wave_direction_range': rangeType,
+        'swell_direction_range': rangeType,
+        'wind_speed_max': {'type': 'int32'},
+        'wave_height_min': {'type': 'double'},
+        'wave_period_min': {'type': 'int32'},
+        'swell_height_min': {'type': 'double'},
+        'swell_period_min': {'type': 'int32'}
     }
 }
 
 DOMAIN = {
-    'users': users
+    'users': users,
+    'alerts': alerts
 }
