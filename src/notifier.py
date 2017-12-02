c4602e0ad885bbe1542ad8918c1c6c86550d82c4
Ben Fichter <benjamin_fichter@alumni.brown.edu>
2017-12-02 13:53:55 -0500

    Add skeleton notifier

---
+++ src/notifier.py
@@ -0,0 +1,13 @@
+from apns2.client import APNsClient
+from apns2.payload import Payload
+
+class Notifier:
+    def __init__(self):
+        self.client = APNsClient('key.pem', use_sandbox=True, use_alternative_port=False)
+        
+    def send(self, token, message, count, isSilent):
+        # TODO utilize isSilent
+        payload = Payload(alert=message, sound="default", badge=count)
+        topic = 'com.bfichter.KookMachine'
+        self.client.send_notification(token, payload, topic)
+            
