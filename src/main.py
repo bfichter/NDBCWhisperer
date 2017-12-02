2662dbedc615ddfa0d249db0cf08957e2761387f
Ben Fichter <benjamin_fichter@alumni.brown.edu>
2017-12-02 16:18:38 -0500

    Actually send a test note

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from scraper import Scraper
 from pymongo import MongoClient
+from notifier import Notifier
 
 def main():
+    #push test
+    notifier = Notifier()
+    notifier.send('5886e435dadfbd10f15df6e8f2c73b7eecb7de9eaa9e5becb31f83894ed707a1', 'Test Message', 1, False)
+    #
     stationID = '44008'
     client = MongoClient('localhost', 27017)
     db = client.ndbc
--- src/notifier.py
+++ src/notifier.py
@@ -3,11 +3,11 @@ from apns2.payload import Payload
 
 class Notifier:
     def __init__(self):
-        self.client = APNsClient('key.pem', use_sandbox=True, use_alternative_port=False)
+        self.client = APNsClient('kmDevPush.pem', use_sandbox=True, use_alternative_port=False)
         
     def send(self, token, message, count, isSilent):
         # TODO utilize isSilent
         payload = Payload(alert=message, sound="default", badge=count)
         topic = 'com.bfichter.KookMachine'
         self.client.send_notification(token, payload, topic)
-            
+         
