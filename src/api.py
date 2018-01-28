8a5002db9f48f3073bbbfbf4eaae9c39b9c1ffde
Ben Fichter <benjamin_fichter@alumni.brown.edu>
2018-01-28 17:31:59 -0500

    Update get reading to scrape if necessary, this is necessary so client can offer hints about what params a buoy may have

--- src/api.py
+++ src/api.py
@@ -2,9 +2,8 @@ from eve import Eve
 from scraper import Scraper
 from ndbcMongoClient import NDBCMongoClient
 
-def pre_buoys_get_callback(request, lookup):
+def scrapeIfNecessary(stationID):
     client = NDBCMongoClient().client
-    stationID = lookup["station_id"]
     db = client.ndbc
     # clean all this up
     # possibly hold onto a db connection outside of eve
@@ -13,12 +12,23 @@ def pre_buoys_get_callback(request, lookup):
         client.close()
         return
     
-    scraper = Scraper(lookup["station_id"], db)
+    scraper = Scraper(stationID, db)
     scraper.scrape()
     client.close()
 
+# This isn't getting used for anything anymore, gonna leave it in case
+# We want to use it for some reason
+def pre_buoys_get_callback(request, lookup):
+    stationID = lookup["station_id"]
+    scrapeIfNecessary(stationID)
+    
+def pre_readings_get_callback(request, lookup):
+    stationID = lookup["station_id"]
+    scrapeIfNecessary(stationID) 
+
 app = Eve()
 app.on_pre_GET_buoys += pre_buoys_get_callback
+app.on_pre_GET_readings += pre_readings_get_callback
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True) # was conflicting with something on 5000
